d9c04a9e545b0df192a6506f2c8bf4c3a163b22f
Sayon Baidya <sayonctg001@gmail.com>
2025-04-25 10:28:49 +0600

    added a c file

--- problem.c
+++ problem.c
@@ -1,10 +1,31 @@
-#include<stdio.h>
-int main()
-{
-    int a,b,c,sum=0;
-    scanf("%d,%d,%d",&a,&b,&c);
+#include <stdio.h>
 
-    sum=(abc)+(bca)+(cab);
+int main() {
+    int n;
+    scanf("%d",&n);
+    int a;
+    scanf("%d",&a);
+    int ar[n+1];
+    int i = 0;
+    int sum=0;
+
+
+    while (a > 0) {
+        ar[i] = a % 10;
+        a = a / 10;
+        i++;
+    }
+
+    for (int j = 0; j < i; j++) {
+        if (sum==0)
+        {
+            sum= ar[j];
+        }
+        else{
+            sum=sum+ar[j];
+        }
+    }
     printf("%d",sum);
 
+    return 0;
 }
